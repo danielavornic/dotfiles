bdb059a53da5768e2d3be6b9c6af78d2db231996
Daniela Vornic <daniela.vornic@gmail.com>
2025-07-16 19:54:26 +0300

    feat(nvim): add nvim surround

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,15 @@
 local plugins = {
   {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     lazy = false,
